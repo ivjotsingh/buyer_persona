00036fd7589db4a035e34f68ab1000c07026a13d
ivjotSingh <ivjotofficial@gmail.com>
2017-07-02 16:33:25 +0530

    initial commmit

---
+++ instabot.py
@@ -0,0 +1,20 @@
+import pylab
+import  requests
+from keys import APP_ACCESS_TOKEN
+BASE_URL = 'https://api.instagram.com/v1/'
+
+
+#function to get user id
+def get_user_id(insta_username):
+
+    request_url = (BASE_URL + 'users/search?q=%s&access_token=%s') % (insta_username, APP_ACCESS_TOKEN)
+    print 'GET request url : %s' % (request_url)
+    user_info = requests.get(request_url).json()
+    if user_info['meta']['code'] == 200:
+        if len(user_info['data']):
+            return user_info['data'][0]['id']
+        else:
+            return None
+    else:
+        print 'Status code other than 200 received!'
+
